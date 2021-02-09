0e92cb7d0f5b00c43b2a1d23740d82d04d4fd94f
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-09 05:45:19 +0000

    checking getUpdates

--- button.py
+++ button.py
@@ -5,10 +5,19 @@ from pprint import pprint
 token = os.environ['token']
 
 def getMe():
-    url_getUpdates = f'https://api.telegram.org/bot{token}/getMe'
-    respons_getMe = requests.get(url=url_getUpdates)
+    url_getMe = f'https://api.telegram.org/bot{token}/getMe'
+    respons_getMe = requests.get(url=url_getMe)
     
     return respons_getMe.json()
 
 
-pprint(getMe())
+# pprint(getMe())
+
+def getupdates():
+    url_getUpdates = f'https://api.telegram.org/bot{token}/getUpdates'
+    respons_getUpdates = requests.get(url=url_getUpdates)
+
+    return respons_getUpdates.json()
+
+
+pprint(getupdates())
