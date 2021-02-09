44e5077309a476a6ffd86d2d1a3d72bfbf58b790
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-09 06:50:24 +0000

    checking getUpdates

--- calc.py
+++ calc.py
@@ -4,4 +4,14 @@ from pprint import pprint
 
 token = os.environ['token']
 
-print(token)
+# print(token)
+
+
+def getupdates():
+    url_getUpdates = f'https://api.telegram.org/bot{token}/getUpdates'
+    respons_getUpdates = requests.get(url=url_getUpdates)
+
+    return respons_getUpdates.json()['result'][-1]['message']
+
+
+pprint(getupdates())
