eb034b9d1e29bdfd7f4b6df81639ad223b24f3f5
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-09 06:42:32 +0000

    checking sendKeyboard

--- button.py
+++ button.py
@@ -31,5 +31,24 @@ def sendMessage(idx, text):
     respons_sendMessage = requests.get(url=url_sendMessage, params=payload)
 
 
-sendMessage(1258594598, 'salom')
+def sendKeyboard(idx, text):
+    url_sendMessage = f'https://api.telegram.org/bot{token}/sendMessage'
+    payload = {
+        'chat_id': idx,
+        'text': text,
+        'reply_markup': {
+            'keyboard': [
+                [{'text': 'button1'}]
+            ],
+            'resize_keyboard': True
+        }
+
+    }
+    respons_sendKeyboard = requests.post(url=url_sendMessage, json=payload)
+
+    pprint(respons_sendKeyboard.json())
+    print(respons_sendKeyboard.url)
+
+
+sendKeyboard(1258594598, 'salom')
 
